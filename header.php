0dbdf04f378b8725b223c5d66b1e68b285499f28
baptisteDEL <75997845+baptisteDEL@users.noreply.github.com>
2021-01-10 15:13:39 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet"  href="/biblio_groupe11_etape3/style/style.css">
+	<link rel="stylesheet"  href="/biblio_groupe11_etape4/style/style.css">
 	<meta charset="utf-8">
 	<link rel="icon" href="/biblio_groupe11_etape3/image/logo.png">
 	<title>Manga References</title>
 </head>
 <body>
+    
+    <?php 
+    $link = mysqli_connect("localhost", "root", "", "bibliotheque");
+    
+    if (!link) {
+        echo "Erreur : Impossible de se connecter à MySQL." . PHP_EOL;
+        echo "Errno de débogage : " . mysqli_connect_errno() . PHP_EOL;
+        echo "Erreur de débogage : " . mysqli_connect_errno() . PHP_EOL;
+        exit;
+    }
 	<header>
-		<h1 class="margin"><a href="/biblio_groupe11_etape3/index.php"><img src="/biblio_groupe11_etape3/image/logo.png" height="120" width="120" alt="Logo du site">Manga References</a></h1>
+		<h1 class="margin"><a href="/biblio_groupe11_etape4/index.php"><img src="/biblio_groupe11_etape3/image/logo.png" height="120" width="120" alt="Logo du site">Manga References</a></h1>
 		<div class="option">
-			<a href="/biblio_groupe11_etape3/contact.php">Contact</a>
+			<a href="/biblio_groupe11_etape4/contact.php">Contact</a>
 		</div>
 		</header>
--- index.php
+++ index.php
@@ -7,9 +7,23 @@
 				</a>	
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : L'Attaque des Titans</li>
-				<li><strong>Ecrivain</strong> : Hajime isayama</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'L'attaque des titans' AND Auteur.idPersonne = 1"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'L'attaque des titan' AND Auteur.idPersonne = 1; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>";               
+                                                                        
 			</ul>  
 		</article>
 		<article>
@@ -17,9 +31,23 @@
 				<a href="redirection/9782871294146.php"><img src="image/Tome_1_Naruto.jpg" height="193" width="132" alt="Tome_1_Naruto"></a>
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : Naruto</li>
-				<li><strong>Ecrivain</strong> : Mashashi Kashimoto</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'Naruto' AND Auteur.idPersonne = 2"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'Naruto' AND Auteur.idPersonne = 2; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>";               
+                                                                        
 			</ul>
 		</article>
 		<article>
@@ -27,9 +55,22 @@
 				<a href="redirection/9783551745811.php"><img src="image/Tome_1_OP.jpg" height="193" width="132" alt="Tome_1_OP"></a>
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : One Piece</li>
-				<li><strong>Ecrivain</strong> : Eiichirō Oda</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'One Piece' AND Auteur.idPersonne = 3"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'One Piece' AND Auteur.idPersonne = 3; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>"; 
 			</ul>
 		</article>
 		<article>
@@ -37,9 +78,22 @@
 				<a href="redirection/9781421582986.php"><img src="image/Tome_1_Tokyo_Ghoul.jpg" height="193" width="132" alt="Tome_1_Tokyo_Ghoul"></a>
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : Tokyo Ghoul</li>
-				<li><strong>Ecrivain</strong> : Sui Ishida</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'Tokyo Ghoul' AND Auteur.idPersonne = 4"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'Tokyo Ghoul' AND Auteur.idPersonne = 4; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>"; 
 			</ul>
 		</article>
 		<article>
@@ -47,9 +101,22 @@
 				<a href="redirection/9781421582696.php"><img src="image/Tome_1_MHA.jpg" height="193" width="132" alt="Tome_1_MHA"></a>
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : My Hero Academia</li>
-				<li><strong>Ecrivain</strong> : Kōhei Horikoshi</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'My Hero Academia' AND Auteur.idPersonne = 5"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'My Hero Academia' AND Auteur.idPersonne = 5; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>"; 
 			</ul>
 		</article>
 		<article>
@@ -57,9 +124,22 @@
 				<a href="redirection/9782871292661.php"><img src="image/Tome_1_HxH.jpg" height="193" width="132" alt="Tome_1_HxH"></a>
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : Hunter x Hunter</li>
-				<li><strong>Ecrivain</strong> : Yoshihiro Togashi</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'Hunter X Hunter' AND Auteur.idPersonne = 6"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'Hunter X Hunter' AND Auteur.idPersonne = 6; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>"; 
 			</ul>
 		</article>
 		<article>
@@ -67,9 +147,22 @@
 				<a href="redirection/9781421587189.php"><img src="image/Tome_1_Black_Clover.jpg" height="193" width="132" alt="Tome_1_Black_Clover"></a>
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : Black Clover</li>
-				<li><strong>Ecrivain</strong> : Yūki Tabata</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'Black Clover' AND Auteur.idPersonne = 7"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'Black Clover' AND Auteur.idPersonne = 7; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>"; 
 			</ul>
 		</article>
 		<article>
@@ -77,9 +170,22 @@
 				<a href="redirection/9782809469295.php"><img src="image/Tome_1_Demon_Slayer.jpg" height="193" width="132" alt="Tome_1_Demon_Slayer"></a>
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : Demon Slayer</li>
-				<li><strong>Ecrivain</strong> : Koyoharu Gotōge</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'Demon Slayer' AND Auteur.idPersonne = 8"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'Demon Slayer' AND Auteur.idPersonne = 8; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>"; 
 			</ul>
 		</article>
 		<article>
@@ -87,9 +193,22 @@
 				<a href="redirection/9782811613563.php"><img src="image/Tome_1_SDS.jpg" height="193" width="132" alt="Tome_1_SDS"></a>
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : Seven Deadly Sins</li>
-				<li><strong>Ecrivain</strong> : Nakaba Suzuki</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'Seven Deadly Sins' AND Auteur.idPersonne = 9"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'Seven Deadly Sins' AND Auteur.idPersonne = 9; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>"; 
 			</ul>
 		</article>
 		<article>
@@ -97,9 +216,22 @@
 				<a href="redirection/9782847897371.php"><img src="image/Tome_1_Baki.jpg" height="193" width="132" alt="Tome_1_Baki"></a>
 			</div>
 			<ul>
-				<li><strong>Nom</strong> : Baki</li>
-				<li><strong>Ecrivain</strong> : Keisuke Itagaki</li>
-				<li><strong>Genre</strong> : Manga</li>
+				<?php $req = "SELECT Genre.libelle, Livre.annee,nom,prenom,isbn FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Editeur ON Livre.editeur.id WHERE Livre.titre = 'Baki' AND Auteur.idPersonne = 10"
+    $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> ISBN : " . $row["isbn"] ."</li>";
+                        echo "<li> Genre : " . $row["libelle"] ."</li>";
+                        echo "<li> Année : " . $row["anneee"] ."</li>";
+                        echo "<li> Auteur : " . $row["nom"] ." " . $row["prenom] ." </li>; 
+                    }
+                    mysqli_free_result($result);
+                } ?> 
+                <?php $req = "SELECT * FROM Auteur JOIN Personne On Auteur.idPersonne = Personne.id JOIN Role ON Auteur.idRole = Role.id JOIN Livre ON Auteur.idLivre = Livre.isbn JOIN Genre ON Livre.genre = Genre.id JOIN Editeur ON Livre.editeur = Editeur.id WHERE Livre.titre = 'Baki' AND Auteur.idPersonne = 10; ";
+     $result = mysqli_query($link, $req);
+                if ($result) {
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        echo "<li> Editeur : " . $row["libelle"] ."</li>"; 
 			</ul>
 		</article>
 		
